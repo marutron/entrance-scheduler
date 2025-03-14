ff025994c50ca4d5e05cb09408c1251db3aa7ac8
marat <ahmatov-marat8@mail.ru>
2025-03-14 10:34:46 +0400

    add orbit_mapper

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ use crate::parser::config_parser::parse_config;
 use crate::parser::file_parser::parse_file;
 use std::error::Error;
 
+mod mapper;
 mod parser;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let path = parse_config()?;
     let position_hash = parse_file(path)?;
 
-    for (key, val) in position_hash {
+    for (key, val) in &position_hash {
         println!("{key} : {val:?}")
     }
     Ok(())
