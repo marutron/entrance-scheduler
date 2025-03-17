2a59f222d37332abee56bd485472ae731d4431ac
marat <ahmatov-marat8@mail.ru>
2025-03-17 08:02:23 +0400

    add tasks parsing

--- src/parser/task.rs
+++ src/parser/task.rs
@@ -8,27 +8,31 @@ pub struct Task {
 
 impl Task {
     pub fn new(task_str: &str) -> Self {
-        let sort_vec: Vec<String> = task_str
-            .split(" ")
+        let pair_vec: Vec<String> = task_str
+            .split_whitespace()
             .filter_map(|s| Option::from(s.trim().to_string()))
             .collect();
-        let sort_1: Vec<String> = sort_vec[0]
+        let sort_1: Vec<String> = pair_vec[0]
             .split("-")
             .filter_map(|s| Option::from(s.trim().to_string()))
             .collect();
-        let sort_2: Vec<String> = sort_vec[0]
-            .split("-")
-            .filter_map(|s| Option::from(s.trim().to_string()))
-            .collect();
-        // todo sort_2 - Option
+        let sort_2 = if let Some(pair) = pair_vec.get(1) {
+            let sort_2: Vec<String> = pair
+                .split("-")
+                .filter_map(|s| Option::from(s.trim().to_string()))
+                .collect();
+            (Some(sort_2[0].clone()), sort_2[1].parse::<u8>().unwrap())
+        } else {
+            (None, 0)
+        };
 
         Task {
             percent_1: sort_1[0].clone(),
             count_1: sort_1[1]
                 .parse::<u8>()
                 .expect("Невозможно распарсить количество ТВС в задании"),
-            percent_2: Some(sort_2[0].clone()),
-            count_2: sort_2[1].parse::<u8>().unwrap(),
+            percent_2: sort_2.0,
+            count_2: sort_2.1,
         }
     }
 }
